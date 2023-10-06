2f347fd5136f7e6a784eba348860c7623c899672
Amrina24 <147151457+Amrina24@users.noreply.github.com>
2023-10-06 16:50:04 +0500

    Add files via upload

---
+++ Q4.js
@@ -0,0 +1,8 @@
+"use strict";
+//Q4: Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+// Your output should  including the quotation marks
+let Name1;
+Name1 = 'Albert Einstein Ones said';
+let Quote1;
+Quote1 = 'A person who never made a mistake never tried anything new';
+console.log(Name1 + "," + " \" " + Quote1 + ". \" ");
