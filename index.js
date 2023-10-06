3a64d5b943810f993ee7ded7380295b22e21a462
Amrina24 <147151457+Amrina24@users.noreply.github.com>
2023-10-06 17:01:03 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let full_name2 = " Amrina Salaar ";
+const rstrip = ("\t" + full_name2);
+const lstrip = ("\t" + full_name2);
+const strip = ("\t" + full_name2);
+console.log(full_name2);
+console.log("Whitespace Stripping:\n\t" + rstrip + lstrip + strip);
---
+++ index.ts
@@ -0,0 +1,6 @@
+let full_name2 = " Amrina Salaar "
+const rstrip = ("\t" + full_name2);
+const lstrip = ("\t" + full_name2)
+ const strip = ("\t" + full_name2)
+console.log(full_name2)
+console.log("Whitespace Stripping:\n\t" + rstrip + lstrip + strip)
